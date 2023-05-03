8f78674a7b28649e98e61ecf87af25123322c2ac
Elias Rocha <57662339+eliasrocha82@users.noreply.github.com>
2023-05-03 18:18:36 +0000

    tt

--- public/main.js
+++ public/main.js
@@ -1,12 +1,2 @@
-import { name, draw, reportArea, reportPerimeter } from '../modules/square.js';
-import randomSquare from '../modules/square.js';
-
-let myCanvas = create('myCanvas', document.body, 480, 320);
-let reportList = createReportList(myCanvas.id);
-
-let square1 = draw(myCanvas.ctx, 50, 50, 100, 'blue');
-reportArea(square1.length, reportList);
-reportPerimeter(square1.length, reportList);
-
-// Use the default 
-let square2 = randomSquare(myCanvas.ctx);
+import {App} from "../modules/App.js
+alert(App())
